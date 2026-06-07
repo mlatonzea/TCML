f34dced7d66d1087c0f8af10fa3ab519d0a520b5
mlatonzea <mlatonzea@gmail.com>
2026-06-06 20:28:10 -0500

    Pushing some github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://mlatonzea.github.io/TCML',
+	base: '/TCML/',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
